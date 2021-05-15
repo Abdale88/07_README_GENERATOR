531ed840e55def629d0c7ce76e33f675adee62b5
Abdale Hassan <cabdale989@gmail.com>
2021-05-15 16:17:55 -0500

    add function to generate a file containing the README file

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+
+function writeToFile(data) {
+    fs.writeFile("readMeGenerator.md", data, (err) =>
+       err ? console.error(err) : console.log("Success"),
+       
+    );
+}
+
