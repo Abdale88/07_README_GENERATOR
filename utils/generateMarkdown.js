9d2635ce8eff521f8ad0a3f3ebecc69ac5bfb054
Abdale Hassan <cabdale989@gmail.com>
2021-05-15 16:15:23 -0500

    add variables to require inquirer and fs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  return `# ${data.license}
+  ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+  `
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -18,3 +22,8 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+module.exports = renderLicenseBadge;
+
+module.exports = {
+  renderLicenseBadge,
+};
