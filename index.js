96192447ecddbd9ec85316114dab4b7e387552c2
Abdale Hassan <cabdale989@gmail.com>
2021-05-15 16:22:08 -0500

    add function which contains desctiopions, installations, license, contribution, and so on

--- index.js
+++ index.js
@@ -11,3 +11,64 @@ function writeToFile(data) {
     );
 }
 
+function readMe(data){
+    const {title, Description, install, usage, license, contribution, testing, github, email, info}  = data;
+   
+    const useData =  
+    `
+## ${title}
+
+## License
+   ${license === 'MIT' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-coral.svg)`: ''}
+   ${license === 'Apache' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-green.svg)`: ''}
+   ${license === 'GPLv2' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`: ''}
+   ${license === 'GPLv3' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`: ''}
+   ${license === 'Unlicense' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-blueviolet.svg)`: ''}
+   ${license === 'LGPLv3' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`: ''}
+   ${license === 'AGPLv3' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`: ''}
+   ${license === 'Other' ? `GitHub License ![GitHub license](https://img.shields.io/badge/license-${license}-yellow.svg)`: ''}
+
+
+## Description
+ ${Description}  
+   
+   ## Table of Contents
+
+   * [Installation](#installation)
+
+   * [Usage](#usage)
+
+   * [License](#license)
+
+   * [Contribution](#contributing)
+
+   * [Testing](#testing)
+
+   * [Questions](#questions)
+
+## Installation
+  ${install}
+
+## Usage
+ ${usage}
+
+## License
+ ${license}
+ 
+
+## Contributing
+ ${contribution}
+
+## Testing
+ ${testing}
+
+
+## Questions
+${info}
+${email}
+![Here is a link to my GitHub]https://github.com/${github}
+     
+    `
+   
+    writeToFile(useData);
+}
